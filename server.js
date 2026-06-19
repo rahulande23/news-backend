41fc943070cf57263e6d661dd3befe3b4b9c7dad
rahulande23-dev <rahul@gnssoftware.in>
2026-06-19 12:40:17 +0530

    cors fixed

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ const newsRoutes = require("./routes/news");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  process.env.FRONTEND_URL, // set this on Render to your Vercel URL
-].filter(Boolean);
-
-app.use(cors({ origin: allowedOrigins }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/news", newsRoutes);
